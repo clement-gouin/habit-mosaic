52a56ec7fa680324939e0a1e6daa1bb984874695
Clément GOUIN <clement.gouin@nameshield.net>
2023-12-13 15:35:25 +0000

    fix code

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Web;
 
 use Illuminate\View\View;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(): View
     {
         return view('dashboard');
     }
