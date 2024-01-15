72aadcc4293f77e44647f478c9af05ac15cbaca9
Clément GOUIN <clement.gouin@nameshield.net>
2024-01-15 14:19:09 +0000

    better motivation banner

--- resources/js/components/day/MotivationBanner.vue
+++ resources/js/components/day/MotivationBanner.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="w-100 border-bottom border-1 py-2 user-select-none m-0 text-center bg-white">
-        <span v-if="score < 0">🧎 You'll do better </span>
-        <span v-else-if="score === 0">🧍 Let's go !</span>
-        <span v-else-if="score < average * 0.75">🚶 Good job ! </span>
-        <span v-else-if="score < average">🏃 Almost there... <small class="text-dark-emphasis">{{ (average - score).toFixed(1) }} more to beat average</small></span>
-        <span v-else-if="score < average * 1.5">🎉 You did it ! <small class="text-dark-emphasis">{{ (score - average).toFixed(1) }} over average</small></span>
-        <span v-else>🏅 You crushed it !! <small class="text-dark-emphasis">{{ (score - average).toFixed(1) }} over average</small></span>
+        {{ level.text }}
+        <small v-if="level.showDiff && average > score" class="text-dark-emphasis">{{ (average - score).toFixed(1) }} more to beat average</small>
+        <small v-else-if="level.showDiff" class="text-dark-emphasis">{{ (score - average).toFixed(1) }} over average</small>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
 interface Props {
     score: number
     average: number
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+interface Level {
+    minScore: number
+    text: string
+    showDiff?: boolean
+}
+
+const LEVELS: Level[] = [
+    { minScore: 3, text: '🏆 Unbelievable !!!!', showDiff: true },
+    { minScore: 2, text: '🎖️ What a day !!!', showDiff: true },
+    { minScore: 1.5, text: '🏅 You crushed it !!', showDiff: true },
+    { minScore: 1, text: '🎉 You did it !', showDiff: true },
+    { minScore: 0.75, text: '🏃 Almost there...', showDiff: true },
+    { minScore: 0.001, text: '🚶 Good job !' },
+    { minScore: 0, text: '🧍 Let\'s go' },
+    { minScore: -Infinity, text: '🧎 You\'ll do better' }
+];
+
+const level = computed<Level>(() => LEVELS.filter(l => props.score >= l.minScore * props.average)[0]);
 </script>
 
 <style scoped>
