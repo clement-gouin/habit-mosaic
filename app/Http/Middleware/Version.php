839d2de25fd09a475c1e54d1a933281fc395e03b
clement-gouin <clement.gouin@nameshield.net>
2024-09-10 08:34:25 +0000

    pint

--- app/Http/Middleware/Version.php
+++ app/Http/Middleware/Version.php
@@ -4,8 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\Response;
 
