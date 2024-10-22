126242fd6893f95be220d76173b9db9686e3b145
clement-gouin <clement.gouin@nameshield.net>
2024-10-22 13:47:40 +0000

    fix unused

--- app/Http/Controllers/Web/GraphViewController.php
+++ app/Http/Controllers/Web/GraphViewController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class GraphViewController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(): View
     {
         return view('graph_view', []);
     }
